40d42a3353e8e92a725989c86228c46f1cbaa388
rpdentrechales <115180489+rpdentrechales@users.noreply.github.com>
2024-12-16 15:53:07 -0300

    Initial commit

---
+++ views/page_1.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import datetime
+
+st.set_page_config(page_title="Sample Page 1", page_icon="💎",layout="wide")
+
+@st.cache_data
+def load_main_dataframe(worksheet):
+
+  conn = st.connection("gsheets", type=GSheetsConnection)
+  df = conn.read(worksheet=worksheet,dtype={"Ad ID": str})
+
+  df['Day'] = pd.to_datetime(df['Day'])
+
+  df["Ad ID"] = df["Ad ID"].astype(str)
+
+  df.drop_duplicates(subset=["Day","Ad ID"],inplace=True)
+
+  conn.update(data=df,worksheet=worksheet)
+
+  return df
+
+@st.cache_data
+def load_aux_dataframe(worksheet,duplicates_subset):
+
+  conn = st.connection("gsheets", type=GSheetsConnection)
+  df = conn.read(worksheet=worksheet)
+  df = df.drop_duplicates(subset=duplicates_subset)
+
+  return df
+
+st.title("Sample Page 1")
