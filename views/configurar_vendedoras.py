38946d179fc6e57b7b6fd6819675308296cc8f47
rpdentrechales <rpdprocorpo@gmail.com>
2024-12-16 19:53:15 +0000

    Updating files

--- views/configurar_vendedoras.py
+++ views/configurar_vendedoras.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import datetime
+from auxiliar.auxiliar import *
 
 st.set_page_config(page_title="RPD - Configurar Vendedoras", page_icon="💎",layout="wide")
 
@@ -14,4 +15,4 @@ vendedora_selecionada = st.selectbox("Selecione uma Vendedora", nome_das_vendedo
 
 id_vendedora = vendedoras_df.loc[vendedoras_df["nome_vendedora"] == vendedora_selecionada, "id_vendedora"].values[0]
 
-st.markdown(f'[Abrir site da vendedora](https://rpd-procorpo.streamlit.app/inserir_vendas?id={id_vendedora})')
+st.markdown(f'[Abrir site da vendedora](https://rpd-procorpo.streamlit.app/visualisar?id={id_vendedora})')
