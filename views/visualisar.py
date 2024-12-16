ef6fde5338227fbdd360c69eb9c2848ade45b467
rpdentrechales <rpdprocorpo@gmail.com>
2024-12-16 20:10:10 +0000

    Updating files

--- views/visualisar.py
+++ views/visualisar.py
@@ -8,8 +8,24 @@ st.set_page_config(page_title="Pró-Corpo - Visualizar Vendas", page_icon="💎"
 
 st.title("Visualizar Vendas")
 
-vendedoras_df = get_dataframe_from_mongodb(collection_name="dados_vendedoras", database_name="rpd_db")
-billcharges_df = get_dataframe_from_mongodb(collection_name="billcharges_db", database_name="dash_midia")
+url_parameters = st.query_params
+error_page = True
 
-st.dataframe(vendedoras_df)
-st.dataframe(billcharges_df)
+if "id" in url_parameters:
+
+  id_vendedora = st.query_params["id"]
+  vendedoras_df = get_dataframe_from_mongodb(collection_name="dados_vendedoras", database_name="rpd_db")
+  vendedoras_df = vendedoras_df.loc[vendedoras_df["id_vendedora"] == id_vendedora]
+  nome_vendedora = vendedoras_df["nome_vendedora"].iloc[0]
+
+  if nome_vendedora:
+    query = {"nome_vendedora": nome_vendedora}
+
+    billcharges_vendedoras_df = get_dataframe_from_mongodb(collection_name="billcharges_db", database_name="dash_midia",query=query)
+    st.tittle(f"Vendas da Vendedora: {nome_vendedora}") 
+    st.dataframe(billcharges_vendedoras_df)
+
+    error_page = False
+
+if error_page:
+  st.tittle("Página não encontrada")
